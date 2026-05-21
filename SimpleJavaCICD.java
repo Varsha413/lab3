592c373d97439a7dffff270d35790fc907201a67
Varsha413 <varsha.srb.s@gmail.com>
2026-05-21 11:54:55 +0530

    Add success message to main method

--- SimpleJavaCICD.java
+++ SimpleJavaCICD.java
@@ -5,6 +5,8 @@ public class SimpleJavaCICD {
     public static void main(String[] args) {
 
         System.out.println("CI/CD Lab Experiment Successful");
+         System.out.println(" Successful");
+        
 
     }
 }
